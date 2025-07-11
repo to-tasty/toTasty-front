6e1eed7b66e4ab2d4b2d199b643f65ae4c38b559
Lee Ye Seo <rosefor2ne@gmail.com>
2025-07-11 11:02:46 +0900

    refactor: Tasting 폴더 추가 및 컴포넌트 이동

--- src/app/shared/ui/Tasting/TastingCard.tsx
+++ src/app/shared/ui/Tasting/TastingCard.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image';
 
-export default function TastingCard({ isActive = true }) {
+interface TastingCardProps {
+  isActive?: boolean;
+}
+
+export default function TastingCard({ isActive = true }: TastingCardProps) {
   return (
     <div
       className={'w-[100px] hover:scale-103 transition-all duration-300 ease-in-out cursor-pointer'}
