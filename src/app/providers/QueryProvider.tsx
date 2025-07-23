c330e7f2ab774ddc49c3478d0d2cebd7a8a8641c
WOOSIK <woosikzero@gmail.com>
2025-07-23 14:06:44 +0900

    refactor: 기존 코드 복원

--- src/app/providers/QueryProvider.tsx
+++ src/app/providers/QueryProvider.tsx
@@ -1,27 +1,10 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useEffect, useState } from 'react';
-import { useUserStore, refreshAccessToken } from '@/entities/user';
-import { registerAxiosInterceptors } from './registerAxiosInterceptors';
+import { useState } from 'react';
 
 export function QueryProvider({ children }: { children: React.ReactNode }): React.JSX.Element {
   const [queryClient] = useState(() => new QueryClient());
-  const accessToken = useUserStore((state) => state.accessToken);
-
-  useEffect(() => {
-    console.log('✅ [zustand] accessToken:', accessToken);
-  }, [accessToken]);
-
-  // 여기서 interceptor 등록
-  useEffect(() => {
-    registerAxiosInterceptors();
-  }, []);
-
-  useEffect(() => {
-    console.log('🔥 QueryProvider useEffect 실행됨');
-    refreshAccessToken();
-  }, []);
 
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
