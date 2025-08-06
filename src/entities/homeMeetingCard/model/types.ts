d53c390f9b38d9732e2b66af90588de6fbc4a49d
chanwoo <chanwoolee74@gmail.com>
2025-08-06 16:42:20 +0900

    feat: homeMeetingCard 타입 추가

--- src/entities/homeMeetingCard/model/types.ts
+++ src/entities/homeMeetingCard/model/types.ts
@@ -1,1 +1,23 @@
-export type { MeetingList as HomeMeetingCard } from '../../meetings';
+export interface HomeMeetingCard {
+  meetingId: number;
+  meetingAuthor: string;
+  meetingTitle: string;
+  location: {
+    sido: string;
+    address: string;
+    detail: string;
+  };
+  participationFee: number;
+  startAt: string;
+  joinEndAt: string;
+  maxParticipants: number;
+  minParticipants: number;
+  currentParticipants: number;
+  tastingDrinkCount: number;
+  isWished: boolean;
+  thumbnailUrl: string;
+  status: string;
+  drinkType: string;
+  participation: { memberId: number; nickname: string; profileImgUrl: string; isHost: boolean }[];
+  isReviewed: boolean;
+}
