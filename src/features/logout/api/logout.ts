24ff001d89eac82cd4d0b714b4d8225cfff5cd0e
dropper-in <dropperin59@gmail.com>
2025-08-08 21:42:45 +0900

    fix: {} 타입 오용 수정 및 export 방식 변경

--- src/features/logout/api/logout.ts
+++ src/features/logout/api/logout.ts
@@ -1,5 +1,5 @@
 import { getApi } from '@/shared/api/axiosApis';
 
-export default async function logout(): Promise<{} | null> {
-  return getApi<any>(`/api/v1/auth/logout`);
+export default async function logout(): Promise<object | null> {
+  return getApi(`/api/v1/auth/logout`);
 }
