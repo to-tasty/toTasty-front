81880efdf417b1cdd17d646032744d684b7dc758
leechanwoo <90599105+BugEliminator@users.noreply.github.com>
2025-08-11 11:16:21 +0900

    Bugfix/#174 로그아웃시 내가 좋아할 모임 & 위시리스트가 보이는 이슈 해결 (#185)
    * fix: 로그아웃시 내가 좋아할 모임이 보이는 이슈
    
    * feat: 비로그인 시 더미 카드 4장 렌더링 추가

--- src/entities/homeMeetingCard/model/hooks/useWishlistQuery.ts
+++ src/entities/homeMeetingCard/model/hooks/useWishlistQuery.ts
@@ -1,7 +1,10 @@
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { useUserStore } from '@/entities/user';
 import homeMeetingCardKeys from '../homeMeetingCard.keys';
 import { HomeMeetingCard } from '../types';
 
 export default function useWishlistMeetingsQuery(): UseQueryResult<HomeMeetingCard[] | null> {
-  return useQuery(homeMeetingCardKeys.wishlist());
+  const userId = useUserStore((state) => state.user?.memberId);
+
+  return useQuery(homeMeetingCardKeys.wishlist(userId));
 }
