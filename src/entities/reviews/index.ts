9dbbe9f60764885f56ad90126e0f8aba547ebd2b
Lee Ye Seo <rosefor2ne@gmail.com>
2025-08-12 23:28:22 +0900

    feat: getMeetingReviews api 추가

--- src/entities/reviews/index.ts
+++ src/entities/reviews/index.ts
@@ -1,2 +1,3 @@
 export { default as useReviewListQuery } from './model/hooks/useReviewListQuery';
 export { default as useReviewDetailQuery } from './model/hooks/useReviewDetailQuery';
+export { default as useMeetingReviewsQuery } from './model/hooks/useMeetingReviewsQuery';
