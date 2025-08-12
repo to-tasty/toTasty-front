03b3896ef955f3c1bbb681ad4824f08c82422939
WOOSIK <woosikzero@gmail.com>
2025-08-12 21:14:07 +0900

    feat: 리뷰작성 2차 개발 - 일부 인터페이스 수정 - 파일 및 기타 이름 오타 수정

--- src/entities/tasting-list/api/getTastingList.ts
+++ src/entities/tasting-list/api/getTastingList.ts
@@ -2,5 +2,14 @@ import { getApi } from '@/shared/api/axiosApis';
 import { TastingList } from '@/entities/tasting-list/model/types';
 
 export default async function getTastingList(meetingId: number): Promise<TastingList | null> {
-  return getApi<TastingList>(`/api/v1/tasting-list/${meetingId}`);
+  const response = getApi<TastingList>(`/api/v1/tasting-list/${meetingId}`);
+  response.then((res) => {
+    res?.tastingList.map((item) => {
+      item.drinkColor ||= '';
+      item.drinkFlavor ||= '';
+      item.drinkTaste ||= '';
+    });
+    return res;
+  });
+  return response;
 }
--- src/widgets/reviews/post/PostReview.tsx
+++ src/widgets/reviews/post/PostReview.tsx
@@ -2,23 +2,52 @@
 
 import { Button } from '@/shared/ui';
 import { useAppForm } from '@/shared/lib';
-import { contentSchema } from './model/validationSchema';
-import reviewOptions from './model/reviewOptions';
-import ReviewImgCardRow from './ui/ReviewImgCardRow';
+import { contentSchema, tastingSchema, flavorSchema, colorSchema } from './model/validationSchema';
+import ReviewImgCardRow from '../ui/ReviewImgCardRow';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/shared/ui';
+import Image from 'next/image';
+import { TastingInfo } from '@/shared';
+import { usePostReviewMutation } from '@/features/reviews';
+import { useRouter } from 'next/navigation';
 
-export default function PostReview({ meetingId }: { meetingId: number }) {
+export default function PostReview({
+  tastingList,
+  meetingId,
+}: {
+  tastingList: TastingInfo[];
+  meetingId: number;
+}) {
+  const router = useRouter();
+  const { mutateAsync } = usePostReviewMutation();
+
   const form = useAppForm({
-    ...reviewOptions,
+    defaultValues: {
+      meetingId: meetingId,
+      reviewRating: 1,
+      reviewContent: '',
+      tastingList: tastingList,
+    },
     validators: {},
-    onSubmit: ({ value }) => {
+    onSubmit: async ({ value }) => {
       console.log(value);
+      const result = await mutateAsync(value);
+      if (result && result.reviewId) {
+        alert('리뷰 작성을 완료하였습니다.');
+        router.push(`/reviews/${result.reviewId}`);
+      }
     },
   });
 
   return (
-    <>
+    <div className="mb-10">
       <div className="flex items-center justify-center min-h-12 text-gray-080 text-3xl font-bold mt-16 border-2 rounded-xl">
-        참여했던 ~~ 모임은 어떠셨나요? 후기를 남겨주세요!
+        참여했던 모임은 어떠셨나요? 후기를 남겨주세요!
       </div>
       <form
         method="post"
@@ -41,16 +70,113 @@ export default function PostReview({ meetingId }: { meetingId: number }) {
             <field.TextareaField
               label="총평"
               placeholder="모임에 대한 설명, 음료에 대한 설명을 작성해주세요."
-              maxLength={3000}
+              maxLength={2000}
               required
               AreaClassName="min-h-[153px]"
             />
           )}
         </form.AppField>
 
-        <ReviewImgCardRow meetingId={meetingId} />
+        <ReviewImgCardRow tastingInfo={tastingList} />
 
-        <div className="flex justify-center mt-4 gap-3">
+        <form.AppField name="tastingList" mode="array">
+          {(field) => (
+            <>
+              <Carousel
+                className="w-full overflow-visible"
+                opts={{ loop: false, align: 'start', containScroll: 'trimSnaps' }}
+                orientation="horizontal"
+              >
+                <CarouselPrevious
+                  className="absolute -left-6 top-1/2 -translate-y-1/2 z-10 transition-shadow duration-300 hover:shadow-md"
+                  style={{ pointerEvents: 'auto' }}
+                />
+                <CarouselContent className="flex">
+                  {field.state.value?.map((item, index) => {
+                    const carKey = `carouselItem-${item.drinkId}`;
+                    return (
+                      <CarouselItem key={carKey}>
+                        <div className="text-xl font-bold text-gray-800 mt-14 ml-4">
+                          {item.drinkName}
+                        </div>
+                        <div className="w-full flex justify-center gap-6 mt-4">
+                          <div className="relative w-[471px] h-[496px] border-2 rounded-2xl">
+                            <Image
+                              src={item.drinkImgUrl ?? ''}
+                              alt="test"
+                              className="w-[471px] h-[496px] rounded-2xl"
+                              fill
+                              style={{
+                                objectFit: 'cover',
+                              }}
+                            />
+                          </div>
+                          <div className="w-[491px] flex flex-col">
+                            <form.AppField
+                              name={`tastingList[${index}].drinkTaste`}
+                              validators={{
+                                onChange: tastingSchema,
+                              }}
+                            >
+                              {(subField) => (
+                                <subField.TextareaField
+                                  label="맛"
+                                  placeholder="해당 음료의 맛에 대해서 작성해주세요."
+                                  maxLength={500}
+                                  required
+                                  AreaClassName="min-h-[100px]"
+                                />
+                              )}
+                            </form.AppField>
+                            <form.AppField
+                              name={`tastingList[${index}].drinkFlavor`}
+                              validators={{
+                                onChange: flavorSchema,
+                              }}
+                            >
+                              {(subField) => (
+                                <subField.TextareaField
+                                  label="향"
+                                  placeholder="해당 음료의 향에 대해서 작성해주세요."
+                                  maxLength={500}
+                                  required
+                                  AreaClassName="min-h-[100px]"
+                                />
+                              )}
+                            </form.AppField>
+                            <form.AppField
+                              name={`tastingList[${index}].drinkColor`}
+                              validators={{
+                                onChange: colorSchema,
+                              }}
+                            >
+                              {(subField) => (
+                                <subField.TextareaField
+                                  label="색"
+                                  placeholder="해당 음료의 색에 대해서 작성해주세요."
+                                  maxLength={500}
+                                  required
+                                  AreaClassName="min-h-[100px]"
+                                />
+                              )}
+                            </form.AppField>
+                          </div>
+                        </div>
+                      </CarouselItem>
+                    );
+                  })}
+                </CarouselContent>
+
+                <CarouselNext
+                  className="absolute -right-6 top-1/2 -translate-y-1/2 z-10 transition-shadow duration-300 hover:shadow-md"
+                  style={{ pointerEvents: 'auto' }}
+                />
+              </Carousel>
+            </>
+          )}
+        </form.AppField>
+
+        <div className="flex justify-center mt-8 gap-3">
           <Button type="button" size="lg" variant="outlinePrimary">
             취소하기
           </Button>
@@ -59,6 +185,6 @@ export default function PostReview({ meetingId }: { meetingId: number }) {
           </Button>
         </div>
       </form>
-    </>
+    </div>
   );
 }
