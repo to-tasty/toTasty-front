546d88014427c8e4a9e867e252b488e074763103
Lee Ye Seo <rosefor2ne@gmail.com>
2025-08-12 11:49:25 +0900

    feat: 모임 취소하기 detailpage에 적용

--- src/entities/meetings/index.ts
+++ src/entities/meetings/index.ts
@@ -2,4 +2,3 @@ export { default as useMeetingListQuery } from './model/hooks/useMeetingListQuer
 export { default as useMeetingDetailQuery } from './model/hooks/useMeetingDetailQuery';
 export type { MeetingFilters, MeetingListInfo, MeetingCardInfo } from './model/types';
 export { SortType } from './model/types';
-//
