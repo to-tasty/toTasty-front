ada178fbe57c7e4a962772411d09d48847cb27c2
Lee Ye Seo <rosefor2ne@gmail.com>
2025-08-13 07:32:47 +0900

    feat: 다크모드 유저 메뉴에 적용

--- src/widgets/navigation-bar/ui/UserDropdown.tsx
+++ src/widgets/navigation-bar/ui/UserDropdown.tsx
@@ -3,13 +3,21 @@
 import { DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem } from '@/shared';
 import Link from 'next/link';
 import { useLogout } from '@/features/logout';
+import { useThemeEffect, useThemeStore } from '@/shared/lib/theme';
+import { Theme } from '@/shared/lib/theme/model/types';
 
 export default function UserDropdown() {
   const { handleLogout } = useLogout();
+  const { theme, toggleTheme } = useThemeStore();
+  useThemeEffect();
+
   return (
     <DropdownMenuContent className="w-30">
       <DropdownMenuGroup>
-        <Link href="/my-page/meetings">
+        <DropdownMenuItem className="cursor-pointer" onClick={toggleTheme}>
+          {theme === Theme.dark ? '다크모드 해제' : '다크모드로 전환'}
+        </DropdownMenuItem>
+        <Link href="/my-page/meetings" className="text-foreground">
           <DropdownMenuItem className="cursor-pointer">마이페이지</DropdownMenuItem>
         </Link>
         <DropdownMenuItem className="cursor-pointer" onClick={handleLogout}>
