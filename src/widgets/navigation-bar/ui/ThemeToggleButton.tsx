9b75784f8f3dd4800b61fc421ed75c89dfc06cb2
Lee Ye Seo <rosefor2ne@gmail.com>
2025-08-13 10:08:20 +0900

    feat: 다크모드 아이콘 변경

--- src/widgets/navigation-bar/ui/ThemeToggleButton.tsx
+++ src/widgets/navigation-bar/ui/ThemeToggleButton.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/shared';
 import { useThemeEffect, useThemeStore } from '@/shared/lib/theme';
 import { Theme } from '@/shared/lib/theme/model/types';
+import { Moon, Sun } from 'lucide-react';
 
 export default function ThemeToggleButton() {
   const { theme, toggleTheme } = useThemeStore();
@@ -13,7 +14,7 @@ export default function ThemeToggleButton() {
       className="px-3 py-2 rounded transition-colors hover:bg-primary/10"
       aria-label="다크모드 토글"
     >
-      {theme === Theme.dark ? '☀️' : '🌙'}
+      {theme === Theme.dark ? <Sun /> : <Moon />}
     </Button>
   );
 }
