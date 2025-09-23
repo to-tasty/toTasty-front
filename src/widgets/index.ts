a50f2efb8b22f398c96c17502a2cce5df0210da1
Lee Ye Seo <rosefor2ne@gmail.com>
2025-09-23 12:15:11 +0900

    refactor: MeetingCard 컴포넌트를 widgets로 이동하고 중복 코드 통합

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,8 +1,7 @@
 export { default as NavigationBar } from './navigation-bar/NavigationBar';
-export { default as FindMeetingList } from './find-meeting/FindMeetingList';
 export { default as FindMeetingHeader } from './find-meeting/FindMeetingHeader';
 export { default as PostMeetingForm } from './post-meeting/PostMeetingForm';
-export { default as FindMeetingCard } from './find-meeting/ui/FindMeetingCard';
+export { MeetingCard, MeetingCardCarousel, MeetingCardList } from './meeting-card';
 export { default as UserProfileCard } from './profile-card/UserProfileCard';
 export { default as PostReview } from './reviews/post/PostReview';
 export { default as MyPageTabs } from './mypage-tabs/ui/MyPageTabs';
---
+++ src/widgets/meeting-card/index.ts
@@ -0,0 +1,3 @@
+export { default as MeetingCard } from './MeetingCard';
+export { default as MeetingCardCarousel } from './MeetingCardCarousel';
+export { default as MeetingCardList } from './MeetingCardList';
