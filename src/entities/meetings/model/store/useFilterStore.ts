ead152c4e8aa5dbe8543e86eadf369badcbb0ab9
Lee Ye Seo <rosefor2ne@gmail.com>
2025-09-26 09:42:43 +0900

    refactor: meetings store 구조를 model/store로 이동

--- src/entities/meetings/model/store/useFilterStore.ts
+++ src/entities/meetings/model/store/useFilterStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { MeetingFilters, SortType } from '../../types';
+import { MeetingFilters, SortType } from '../types';
 
 interface Filters {
   filters: MeetingFilters;
